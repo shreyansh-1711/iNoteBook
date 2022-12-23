1a4f72d475e55778b65dec3638efcf2f578ef6b5
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2022-12-23 08:27:41 +0530

    auth token created

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 5000
+const port = 5000;
 app.use(express.json());
 
 // Avaiable Routes 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,37 +2,63 @@ const express = require('express');
 const User = require('../model/User')
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Shreyanshisop';
 
 
-router.post('/', [
-    body('name','Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atlast 5 charcater').isLength({ min: 5 }),
-],async (req, res) => {
-    //If there are errors then return Bad request and error
-    console.log(res.body);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atlast 5 charcater').isLength({ min: 5 }),
+], async (req, res) => {
+  //If there are errors then return Bad request and error
+  console.log(res.body);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+
 
     //check weather the user with email exists already
-    let user = User.findOne({email : req.body.email});
-    if(user) {
-        return res.status(400).json({ errors: 'Email with this already exists' });
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ errors: 'Email with this already exists' });
     }
 
+
+    const salt = await bcrypt.genSalt(10);
+    secPass = await bcrypt.hash(req.body.password, salt);
+
+
+    //craete a new user
+
     user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-     
-        
-      })
-      
-    //   .then(user => res.json(user))
-    // .catch(err=>{console.log(err)
-    // res.json({error: 'pLEASE Enter valid email'})})
+      name: req.body.name,
+      email: req.body.email,
+      password: secPass,
+    })
+
+    const data = {
+      user : {
+        id : user.id
+      }
+    }
+
+    var authtoken = jwt.sign(data, JWT_SECRET);
+    // console.log(jwtData);
+    // res.json(user)
+    res.json({authtoken})
+
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error occured");
+  }
 
 
 })
