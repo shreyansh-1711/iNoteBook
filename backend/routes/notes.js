f69dc5a5a68b2a053d1b41b8bcf22e35ab518b82
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2022-12-27 23:29:50 +0530

    delete endpoint news added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -47,23 +47,50 @@ router.post('/addnote', fetchuser, [
 
 
 // Route-3 Update the existing Note
-router.put('/updatenote/:id', fetchuser,  async (req, res) => {
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
     const { title, description, tag } = req.body;
     // create new note 
     const newNote = {};
-    if(title) {newNote.title = title};
-    if(description) {newNote.description = description};
-    if(tag) {newNote.tag = tag};
+    if (title) { newNote.title = title };
+    if (description) { newNote.description = description };
+    if (tag) { newNote.tag = tag };
 
     let note = await Note.findById(req.params.id);
-    if(!note){
+    if (!note) {
         return res.status(404).send("Not Found");
     }
-    if(note.user.toString() != req.user.id ){
+    if (note.user.toString() != req.user.id) {
         return res.status(401).send("Not Allowed")
     }
-    
-    note = await Note.findByIdAndUpdate(req.params.id , {$set: newNote}, {new: true})
-    res.json({note})
+
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json({ note })
 })
+
+
+// Route-3 DELETE THE NOTE 
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+
+
+        const { title, description, tag } = req.body;
+
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not Found");
+        }
+
+
+        if (note.user.toString() != req.user.id) {
+            return res.status(401).send("Not Allowed")
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Success": "Note has been deleted ", note: note })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server Error occured");
+    }
+})
+
 module.exports = router
