6de9d8b8964633a85d76df748296fe410b1d4b50
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2022-12-28 18:00:52 +0530

    navbar and routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>SHREYANSH</h1>
-    </div>
+    <>
+
+
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+          </Routes>
+        </div>
+      </Router>
+
+
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Thi is homeee
+    </div>
+  )
+}
+
+export default Home
