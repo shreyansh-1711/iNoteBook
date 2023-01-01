df3f72fd0b17a8dccd2ecd0ba4c7a2f31f71b9da
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-01-01 19:27:11 +0530

    delete node added

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const NoteState = (props) => {
 
-const noteInitial = [
+  const noteInitial = [
     {
       "_id": "63aa760407d62f3adfe9e5c5",
       "user": "63a4529db4657e528eded938",
@@ -26,29 +26,41 @@ const noteInitial = [
     }
   ]
 
- const [notes, setNotes] = useState(noteInitial)
+  const [notes, setNotes] = useState(noteInitial)
 
- // Add a note
- const addNote = () => {
+  
+      // Add a Note
+      const addNote = (title, description, tag)=>{
+        // TODO: API Call
+        console.log("Adding a new note")
+        const note = {
+          "_id": "61322f119553781a8ca8d0e08",
+          "user": "6131dc5e3e4037cd4734a0664",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2021-09-03T14:20:09.668Z",
+          "__v": 0
+        };
+        setNotes(notes.concat(note)) 
+      }
 
- }
+      // Delete a Note
+      const deleteNote = (id)=>{
+        console.log("Deleting" + id)
+        const newNotes = notes.filter((note)=>{return note._id !== id})
+        setNotes(newNotes) ;
+      }
+      // Edit a Note
+      const editNote = (id, title, description, tag)=>{
 
- // Delete a note
- const deleteNote = (id) => {
+      }
 
- }
-
-
- // Edit a note
- const editNote = (id) => {
-
- }
-
-return(
-    <NoteContext.Provider value= {{notes, addNote, deleteNote, editNote}}>
-            {props.children}
+  return (
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
+      {props.children}
     </NoteContext.Provider>
-)
+  )
 }
 
 
